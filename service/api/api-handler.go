80b0d3adcc31feab8be0571be520bd6095566a05
Manuel Rosales Maquieyra <manuelrosalesmaquieyra@Mac.wind3.hub>
2025-01-12 21:31:23 +0100

    feat(api): implement user handlers to match database changes
    - Move setMyUserName to user-handlers.go
    - Added setMyPhoto handler
    - Added getUserConversations handler
    - Added getUserFromToken

--- service/api/api-handler.go
+++ service/api/api-handler.go
@@ -7,6 +7,10 @@ import (
 // Handler returns an instance of httprouter.Router that handle APIs registered here
 func (rt *_router) Handler() http.Handler {
 	// Register routes
+	rt.router.PUT("/users/{username}", rt.setMyUserName)
+	rt.router.POST("/users/{username}/photo", rt.setMyPhoto)
+	rt.router.GET("/users/{username}/conversations", rt.getUserConversations)
+
 	rt.router.GET("/", rt.getHelloWorld)
 	rt.router.GET("/context", rt.wrap(rt.getContextReply))
 
